1436d1de86a9c4eac841b1967bed485b994654b9
fauer4effect <kyle.fauer@gmail.com>
2018-11-19 10:55:38 -0500

    list data structure and methods

---
+++ src/data_structures.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+#include "data_structures.h"
+
+int list_length(node_t *list)
+{
+    if (list->data == NULL)
+        return 0;
+    
+    int length = 1;
+    while (list->next != NULL)
+    {
+        length++;
+        list = list->next;
+    }
+    return length;
+}
+
+int append_to_list(void *new_data, node_t *list)
+{
+    node_t *new_node = malloc(sizeof(node_t));
+    if (new_node == NULL)
+    {
+        // malloc fail
+        return -1;
+    }
+    new_node->data = new_data;
+    new_node->next = NULL;
+    while (list->next != NULL)
+    {
+        list = list->next;
+    }
+    list->next = new_node;
+    return 0;
+}
+
+int clear_list(node_t *list)
+{
+    int length = list_length(list);
+    node_t *next_node;
+    while (list->next != NULL)
+    {
+        next_node = list->next;
+        free(list->data);
+        free(list);
+        list = next_node;
+    }
+    return 0;
+}
--- src/data_structures.h
+++ src/data_structures.h
@@ -12,3 +12,12 @@ typedef struct {
     Globally_Ordered_Update *global_ordered_update;
                                 // ordered update for this sequence number, if any
 } Global_Slot;
+
+typedef struct node {
+    void *data;
+    struct node *next;
+} node_t;
+
+int list_length(node_t *list);
+int append_to_list(void *new_data, node_t *list);
+int clear_list(node_t *list);
