f9c8bdf56d1931c4691dadbe734e47eb3b3952bb
fauer4effect <kyle.fauer@gmail.com>
2018-11-23 15:54:21 -0500

    networking setup

--- src/main.c
+++ src/main.c
@@ -174,4 +174,74 @@ int main(int argc, char *argv[])
     }
 
     logger(0, LOG_LEVEl, PROCESS_ID, "Command line parsed\n");
+
+    // networking schtuff
+    fd_set master;                          // master file descriptor list
+    fd_set read_fds;                        // temp file descriptor list for select()
+    int fdmax;                              // maximum file descriptor num
+
+    int listener;                           // listening socket descriptor
+    int newfd;                              // newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr;     // client address
+    socklen_t addrlen;    
+
+    int nbytes;
+
+    int yes = 1;                            // for setsockopt() SO_REUSEADDR
+    int i, rv;
+
+    struct addrinfo hints, *ai, *p;
+
+
+    FD_ZERO(&master);                       // clear master and temp sets
+    FD_ZERO(&read_fds);
+
+    // get us a socket and bind it
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0)
+    {
+        logger(1, LOG_LEVEL, PROCESS_ID, "selectserver: %s\n", gai_strerror(rv));
+        exit(1);
+    }
+    
+    for (p = ai; p != NULL; p = p->ai_next)
+    {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (listener < 0)
+        {
+            continue;
+        }
+
+        // prevent "address already in use"
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0)
+        {
+            close(listener);
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL)
+    {
+        logger(1, LOG_LEVEL, PROCESS_ID, "selectserver: failed to bind\n");
+        exit(1);
+    }
+    freeaddrinfo(ai);
+
+    if (listen(listener, 10) == -1)
+    {
+        logger(1, LOG_LEVEL, PROCESS_ID, "listen fail\n");
+        exit(1);
+    }
+
+    // add listener to master set
+    FD_SET(listener, &master);
+    fdmax = listener;
+
+    logger(0, LOG_LEVEL, PROCESS_ID, "Networking setup complete\n");
 }
