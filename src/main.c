31902e97c2a2c48c94ea3b2e9df7b808fec2541b
fauer4effect <kyle.fauer@gmail.com>
2018-11-26 17:26:38 -0500

    fix incomplete types

--- src/main.c
+++ src/main.c
@@ -35,25 +35,25 @@ int MAX_CLIENT_ID = 32; // size of arrays that are indexed by client id
 int MY_SERVER_ID; // unique identifier for this server
 int NUM_PEERS;    // how many peers in the protocol
 char *PORT;       // what port is it listening on
-char *PEERS[];    // who all the list of peers
+char **PEERS;     // who all the list of peers
 int STATE;        // one of {LEADER_ELECTION, REG_LEADER, REG_NONLEADER}
                   // defined in data_structures.h
 
 // view state variables
 int LAST_ATTEMPTED; // last view this server attempted to install
 int LAST_INSTALLED; // last  view this server installed
-View_Change *VC[];  // array of View_Change messages, indexed by server_id
+View_Change **VC;   // array of View_Change messages, indexed by server_id
                     // size of NUM_PEERS
 
 // prepare phase variables
 Prepare *PREPARED;      // the Prepare message from last preinstalled view, if any
-Prepare *PREPARE_OKS[]; // array of Prepare_OK messages received, indexed by serv id
+Prepare **PREPARE_OKS;  // array of Prepare_OK messages received, indexed by serv id
                         // size of NUM_PEERS
 
 // global ordering variables
 int LOCAL_ARU;                 // local aru value of this server
 int LAST_PROPOSED;             // last sequence number proposed by the leader
-Global_Slot *GLOBAL_HISTORY[]; // array of global_slots indexed by sequence number
+Global_Slot **GLOBAL_HISTORY;  // array of global_slots indexed by sequence number
                                // size is dynamic by increase_array_size()
                                // defined in data_structures.h
 
@@ -61,7 +61,7 @@ Global_Slot *GLOBAL_HISTORY[]; // array of global_slots indexed by sequence numb
 struct timeval PROGRESS_TIMER; // timeout on making global progress
 bool PROGRESS_TIMER_SET;       // keep track of whether the progress time was set
 uint32_t PROGRESS_TIMEOUT;     // how long to wait until PROGRESS_TIMER is timed out
-uint32_t *UPDATE_TIMER[];      // array of timeouts, indexed by client id
+uint32_t **UPDATE_TIMER;       // array of timeouts, indexed by client id
                                // dynamic size by increase_array_size()
 uint32_t UPDATE_TIMEOUT;       // how long to wait until UPDATE_TIMER is timed out
 
@@ -71,7 +71,7 @@ uint32_t *LAST_EXECUTED;          // array of timestamps, indexed by client_id
 uint32_t *LAST_ENQUEUED;          // array of timestamps, indexed by client_id
                                   // stores tv_sec field in struct timeval
                                   // dynamic size
-Client_Update *PENDING_UPDATES[]; // array of Client_Update messages, indexed by client_id
+Client_Update **PENDING_UPDATES;  // array of Client_Update messages, indexed by client_id
                                   // dynamic size
 
 // when initializing node_t objects (really all of the structs)
